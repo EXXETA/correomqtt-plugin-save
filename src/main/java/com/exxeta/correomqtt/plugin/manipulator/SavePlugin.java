868584a62cac47be099bd54a6cac4f7fb33da44b
Reinhold, Frieder <frieder.reinhold@exxeta.com>
2020-01-31 09:55:23 +0100

    initial commit

---
+++ src/main/java/com/exxeta/correomqtt/plugin/manipulator/SavePlugin.java
@@ -0,0 +1,23 @@
+package com.exxeta.correomqtt.plugin.manipulator;
+
+import com.exxeta.correomqtt.plugin.manager.PermissionPlugin;
+import javafx.util.FXPermission;
+import org.pf4j.PluginWrapper;
+
+import java.io.FilePermission;
+import java.security.Permissions;
+
+public class SavePlugin extends PermissionPlugin {
+
+    public SavePlugin(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Override
+    public Permissions getPermissions() {
+        Permissions permissions = new Permissions();
+        permissions.add(new FXPermission("accessWindowList"));
+        permissions.add(new FilePermission("<<ALL FILES>>", "write"));
+        return permissions;
+    }
+}
